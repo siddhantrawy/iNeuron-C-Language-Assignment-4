5a7039d5902406e6f119edc01465782706fb54fd
Siddhant Rawy <96207461+siddhantrawy@users.noreply.github.com>
2022-11-16 00:16:21 +0530

    Add files via upload

---
+++ A4Q1.c
@@ -0,0 +1,11 @@
+// Write a program to print MySirG 5 times on the screen
+
+#include<stdio.h>
+
+int main()
+{
+    char c[10]="MySirG";
+    for(int i=0; i<5; i++)
+        printf("%s\n",c);
+    return 0;
+}
